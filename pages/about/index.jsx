858d44db5c43b21dac3faba712bf77246842f805
mhammadshami <mhammadshami3333@gmail.com>
2024-03-05 16:58:30 +0200

    changes

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -97,6 +97,9 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../variants";
 
+// counter
+import CountUp from "react-countup";
+
 const About = () => {
   const [index, setIndex] = useState(0);
 
@@ -118,7 +121,16 @@ const About = () => {
       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
         {/* text */}
         <div className="flex-1 flex flex-col justify-center">
-          <h2>Captivating <span>stories</span></h2>
+          <h2 className="h2">
+            Captivating <span>stories</span> birth magnificent designs.
+          </h2>
+          <p className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 ">
+            10 years ago, I began freelancing as a developer. Sine then, I've
+            done remote work for agencies, consulted for startups, and
+            collaborated on digital products for business and consumer use.
+          </p>
+          {/* counters */}
+          <div>counters</div>
         </div>
         {/* info */}
         <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
@@ -141,7 +153,10 @@ const About = () => {
           <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start ">
             {aboutData[index].info.map((item, itemIndex) => {
               return (
-                <div key={itemIndex} className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60">
+                <div
+                  key={itemIndex}
+                  className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
+                >
                   {/* title */}
                   <div className="font-light mb-2 md:mb-0 ">{item.title}</div>
                   <div class="hidden md:flex">-</div>
@@ -149,7 +164,11 @@ const About = () => {
                   <div className="flex gap-x-4">
                     {/* icons */}
                     {item.icons?.map((icon, itemIndex) => {
-                      return <div className="text-2xl text-white" key={itemIndex}>{icon}</div>;
+                      return (
+                        <div className="text-2xl text-white" key={itemIndex}>
+                          {icon}
+                        </div>
+                      );
                     })}
                   </div>
                 </div>
