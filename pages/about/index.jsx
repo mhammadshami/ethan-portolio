48ea1f3367f50e10e372076fc00fb58466a950b0
Mohammad Alshami <elshami312@gmail.com>
2024-03-07 18:33:50 +0200

    changes

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-key */
 // icons
 import {
   FaHtml5,
@@ -24,18 +23,22 @@ const aboutData = [
       {
         title: "Web Development",
         icons: [
-          <FaHtml5 />,
-          <FaCss3 />,
-          <FaJs />,
-          <FaReact />,
-          <SiNextdotjs />,
-          <SiFramer />,
-          <FaWordpress />,
+          <FaHtml5 key={1} />,
+          <FaCss3 key={2} />,
+          <FaJs key={3} />,
+          <FaReact key={4} />,
+          <SiNextdotjs key={5} />,
+          <SiFramer key={6} />,
+          <FaWordpress key={7} />,
         ],
       },
       {
         title: "UI/UX Design",
-        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+        icons: [
+          <FaFigma key={1} />,
+          <SiAdobexd key={2} />,
+          <SiAdobephotoshop key={3} />,
+        ],
       },
     ],
   },
